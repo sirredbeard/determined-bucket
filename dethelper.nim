2aa6ceb1f276f03026a53af4d51903d8760dfab8
Hayden Barnes <hayden.barnes@hpe.com>
2024-01-09 17:37:30 -0500

    Delete dethelper.nim

--- dethelper.nim
+++
@@ -1,8 +0,0 @@
-import os, sequtils, strutils
-var
-    detExec = "det "
-    argsSeq = commandLineParams()
-    argsStr: string
-argsStr = (argsSeq.mapIt($it).join)
-detExec.add(argsStr)
-discard execShellCmd(detExec)
